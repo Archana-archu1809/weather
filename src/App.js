f41c7fe2a1f1ecd307d44abf189014bc30e5c1a9
Archu <archu0920@gmail.com>
2022-03-21 18:13:25 +0530

    showing name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from "./logo.svg";
+import { Card } from "antd";
 import "./App.css";
 import Datas from "./components/dataComponent";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
+const API_endpoint = "https://api.openweathermap.org/data/2.5/weather?";
+const API_key = "d52baa827621023548c5a61511d4c43f";
 function App() {
-  const API_endpoint = "https://api.openweathermap.org/data/2.5/weather?";
-  const API_key = "d52baa827621023548c5a61511d4c43f";
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState([]);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
     });
-    console.log(
-      `${API_endpoint}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${API_key}`
-    );
-    let finalAPIEndPoint = `${API_endpoint}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${API_key}`;
+
+    let finalAPIEndPoint = `${API_endpoint}lat=${latitude}&lon=${longitude}&appid=${API_key}`;
     axios
       .get(finalAPIEndPoint)
       .then((response) => {
@@ -29,7 +29,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <h1>{weather.weather}</h1>
+      <h1>{weather.name}</h1>
     </div>
   );
 }
