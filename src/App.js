8791835744c2a25267f519cdf371e9bf26492b2e
Archu <archu0920@gmail.com>
2022-03-22 14:41:30 +0530

    weather changes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ const API_key = "d52baa827621023548c5a61511d4c43f";
 function App() {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
-  const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState({});
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
     });
-
+    console.log(
+      "${API_endpoint}lat=${latitude}&lon=${longitude}&appid=${API_key}"
+    );
     let finalAPIEndPoint = `${API_endpoint}lat=${latitude}&lon=${longitude}&appid=${API_key}`;
     axios
       .get(finalAPIEndPoint)
@@ -27,9 +29,15 @@ function App() {
         console.log(error);
       });
   }, []);
+
   return (
     <div className="App">
-      <h1>{weather.name}</h1>
+      <h1>Weather reports</h1>
+      <h3>{weather.name}</h3>
+      <h3>main:{weather.weather[0].main}</h3>
+
+      <h3>Icon:{weather.weather[0].icon}</h3>
+      <h3>Description:{weather.weather[0].description}</h3>
     </div>
   );
 }
